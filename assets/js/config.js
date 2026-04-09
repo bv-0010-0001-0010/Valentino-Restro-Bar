40aa6d08218204802fbda531a43d36d57d7ac2eb
bv-0010-0001-0010 <bkomuravelli@hotmail.com>
2026-04-09 21:59:21 +1000

    Add reservation config for production

---
+++ assets/js/config.js
@@ -0,0 +1,3 @@
+// config.js - Google Apps Script deployment URL
+// After deploying your Google Apps Script, paste the deployment URL here.
+window.GOOGLE_APPS_SCRIPT_URL = "https://script.google.com/macros/s/AKfycbztcxuiRUxFnfWYXmoM1V-S1GdyDo0k4LST7SGh8Axf5X-wUy5aK5Kj2ASFBCOqT8uAdw/exec";
